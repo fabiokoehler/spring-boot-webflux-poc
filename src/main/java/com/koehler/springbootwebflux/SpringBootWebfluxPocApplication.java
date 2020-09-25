2fc7501a1c072b76d7ed7357a8c9212fb7177d96
Fabio Koehler <fabiokoehler@gmail.com>
2020-09-24 23:27:37 -0300

    Adding initial project structure

---
+++ src/main/java/com/koehler/springbootwebflux/SpringBootWebfluxPocApplication.java
@@ -0,0 +1,13 @@
+package com.koehler.springbootwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koehler/springbootwebflux/SpringBootWebfluxPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koehler.springbootwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

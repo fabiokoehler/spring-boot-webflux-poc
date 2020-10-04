9ebdc995c1307c0b732e4f72cf5888dae4fcf581
Fabio Koehler <fabiokoehler@gmail.com>
2020-10-04 12:20:00 -0300

    Flux and Mono transformation

--- src/test/java/com/koehler/springbootwebflux/fluxandmono/FluxAndMonoTransformationTest.java
+++ src/test/java/com/koehler/springbootwebflux/fluxandmono/FluxAndMonoTransformationTest.java
@@ -2,14 +2,47 @@ package com.koehler.springbootwebflux.fluxandmono;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FluxAndMonoTransformationTest {
 
     List<String> stringFlux = Arrays.asList("A", "B", "C", "D", "E", "F");
+
+    @Test
+    public void fluxWithParallel() {
+        Flux<String> stringFlux = Flux
+                .fromIterable(this.stringFlux)
+                .window(2)
+                .flatMap(stringFlux1 -> stringFlux1.map(this::convertToList).subscribeOn(parallel()))
+                .flatMap(Flux::fromIterable)
+                .log();
+
+        StepVerifier
+                .create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxWithParallelSequential() {
+        Flux<String> stringFlux = Flux
+                .fromIterable(this.stringFlux)
+                .window(2)
+                .flatMapSequential(stringFlux1 -> stringFlux1.map(this::convertToList).subscribeOn(parallel()))
+                .flatMap(Flux::fromIterable)
+                .log();
+
+        StepVerifier
+                .create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
 
     @Test
     public void flux() {
@@ -33,6 +66,6 @@ public class FluxAndMonoTransformationTest {
             e.printStackTrace();
         }
 
-        return Arrays.asList(s, "newValue");
+        return Arrays.asList(s, s + " - newValue");
     }
 }

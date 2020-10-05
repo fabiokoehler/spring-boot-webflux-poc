6d0a0f624832d09b645c1e5b8e21027c6af5566a
Fabio Koehler <fabiokoehler@gmail.com>
2020-10-04 22:22:02 -0300

    Flux with time control

---
+++ src/test/java/com/koehler/springbootwebflux/fluxandmono/FluxAndMonoTimeTest.java
@@ -0,0 +1,33 @@
+package com.koehler.springbootwebflux.fluxandmono;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FluxAndMonoTimeTest {
+
+    @Test
+    public void infiniteSequence() throws InterruptedException {
+        Flux<Long> infiniteFlux = Flux.interval(Duration.ofMillis(100))
+                .log();
+
+        infiniteFlux.subscribe(aLong -> System.out.println("Value: " + aLong));
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void finiteSequenceTest() throws InterruptedException {
+        Flux<Long> finiteFlux = Flux.interval(Duration.ofMillis(100))
+                .take(3)
+                .log();
+
+        StepVerifier.create(finiteFlux)
+                .expectSubscription()
+                .expectNext(0L, 1L, 2L)
+                .verifyComplete();
+    }
+
+}
